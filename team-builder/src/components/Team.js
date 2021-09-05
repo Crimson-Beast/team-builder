b9eba89ae690c2c95f2b53548bdf47b6dadc92a7
Daniel <dek.kneale@gmail.com>
2021-09-05 16:51:34 -0700

    MVP Complete

---
+++ team-builder/src/components/Team.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Team(props) {
+    const {details} = props
+
+    return (
+        <div>
+            <h2>{details.name}</h2>
+            <p>Email: {details.email}</p>
+            <p>Role: {details.role}</p>
+        </div>
+    )
+}
+
+export default Team;
